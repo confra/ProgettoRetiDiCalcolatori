92fce208a5096a5df81c6dddba4a336644cc9e1b
Alessia18 <alessiaangelone18@gmail.com>
2022-06-27 11:43:15 +0200

    Modifiche Docker Compose

--- containers/node/app.js
+++ containers/node/app.js
@@ -1,3 +1,5 @@
+const PORT = 3000;
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -36,6 +38,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
 });
 
 module.exports = app;
